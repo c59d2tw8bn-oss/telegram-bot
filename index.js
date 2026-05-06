da74c0dedcd8662fd606d762d53c1478c63796e4
c59d2tw8bn-oss <c59d2tw8bn@privaterelay.appleid.com>
2026-05-07 02:54:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 const LINK_REF = "https://t.me/Yuicsa_bot?start=locketref_7936179657";
 
 // BƯỚC 1: CHỈ HIỆN 1 NÚT THAM GIA
-bot.start((ctx) => {hi
+bot.start((ctx) => {
+  ctx.reply("👋 Chào mừng bạn!\n\nBấm nút dưới đây để tham gia và nhận quà nhé:", 
+    Markup.inlineKeyboard([[Markup.button.callback("➡️ Tham gia ngay", "phat_mot")]])
+  );
+});
+
 // BƯỚC 2: CÚ BẤM "THẦN THÁNH"
 bot.action("phat_mot", (ctx) => {
   // 1. NHẢY QUA BOT 2 NGAY LẬP TỨC (Dùng lệnh này sẽ tắt luôn cái xoay vòng 'Đang tải')
